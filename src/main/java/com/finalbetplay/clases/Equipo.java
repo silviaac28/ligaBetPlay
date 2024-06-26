61a98589d87bc0b39f539111a4e0e4a8331d1027
Tsammet <carlos61658011@gmail.com>
2024-06-26 17:06:14 -0500

    jugador

--- src/main/java/com/finalbetplay/clases/Equipo.java
+++ src/main/java/com/finalbetplay/clases/Equipo.java
@@ -1,15 +1,9 @@
 package com.finalbetplay.clases;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Equipo {
     private int id;
     private String nombre;
     private String ciudad;
-    private List<Jugador> lstJugadores = new ArrayList<>();
-    private List<Partido> lstPartidos = new ArrayList<>();
 
-
-
+    
 }
---
+++ src/main/java/com/finalbetplay/clases/Jugador.java
@@ -0,0 +1,13 @@
+package com.finalbetplay.clases;
+
+public class Jugador {
+    private int id;
+    private String nombre;
+    private int edad;
+    private String posicion;
+    private String nacionalidad;
+    private int numeroCamiseta;
+
+
+
+}
